3eb5d427c0954b2e3422a29da2bec521c6083272
Giulia Kades <107154732+giuliakadess@users.noreply.github.com>
2023-10-06 20:46:39 -0300

    QUIZ
    adicionando os arquivos do quiz

---
+++ quiz/questions.js
@@ -0,0 +1,34 @@
+export default [
+    {
+        question: "Quanto é 2 + 2?",
+        answers: [
+            {option: "5", correct: false},
+            {option: "4", correct: true},    
+            {option: "6", correct: false}
+        ],
+    },
+    {
+        question: "Quanto é 4 - 2?",
+        answers: [
+            {option: "3", correct: false},
+            {option: "1", correct: false},    
+            {option: "2", correct: true}
+        ],
+    },
+    {
+        question: "Quanto é 2 x 2?",
+        answers: [
+            {option: "4", correct: true},
+            {option: "9", correct: false},    
+            {option: "6", correct: true}
+        ],
+    },
+    {
+        question: "Quanto é 4 + 10?",
+        answers: [
+            {option: "9", correct: false},
+            {option: "14", correct: true},    
+            {option: "12", correct: false}
+        ],
+    },
+];
